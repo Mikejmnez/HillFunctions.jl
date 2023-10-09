840467619e391b2c934acb24f87aea63360ed4b6
Documenter.jl <documenter@juliadocs.github.io>
2023-10-09 03:42:42 +0000

    build based on 631eae3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = HillFunctions","category":"page"},{"location":"#HillFunctions","page":"Home","title":"HillFunctions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for HillFunctions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [HillFunctions]","category":"page"}]
+}
