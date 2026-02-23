8a46e1010d2e21f998b61c758401ee604549121f
Mikejmnez <mjimenez@opendap.org>
2026-02-22 21:55:03 -0800

    some more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -91,7 +91,6 @@ end
 @testset "odd matrix q=1i" begin
     alphas = [1, 2, 3, 4, 5, 6, 7, 8]
     q = 1im
-    Klin = 1:length(alphas)
     N = 5
     B = odd_matrix(q, N, alphas)
 
@@ -110,7 +109,6 @@ end
 @testset "even matrix q=1i" begin
     alphas = [1, 2, 3, 4, 5, 6]
     q = 1im
-    Klin = 1:length(alphas)
     N = 4
     A = even_matrix(q, N, alphas)
 
@@ -124,4 +122,45 @@ end
     @test size(A) == (4,4)
     @test isapprox(A, A_expected; atol=1e-4, rtol=1e-4)
 
+end
+
+@testset "even_matrix preserves Complex{BigFloat} element type with q" begin
+    N = 4
+    q = Complex{BigFloat}(0, 1)
+    alphas = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    A = even_matrix(q, N, alphas)
+
+    @test eltype(A) == Complex{BigFloat}
+end
+
+@testset "even_matrix preserves Complex{BigFloat} element type with q and alphas" begin
+    N = 4
+    q = Complex{BigFloat}(0, 1)
+    alphas = BigFloat[1,2,3,4,5,6]
+
+    A = even_matrix(q, N, alphas)
+
+    @test eltype(A) == Complex{BigFloat}
+end
+
+
+@testset "odd_matrix preserves Complex{BigFloat} element type with q and alphas" begin
+    N = 5
+    q = Complex{BigFloat}(0, 1)
+    alphas = BigFloat[1,2,3,4,5,6,7,8]
+
+    B = odd_matrix(q, N, alphas)
+
+    @test eltype(B) == Complex{BigFloat}
+end
+
+@testset "odd_matrix preserves Complex{BigFloat} element type" begin
+    N = 5
+    q = Complex{BigFloat}(0, 1)
+    alphas = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    B = odd_matrix(q, N, alphas)
+
+    @test eltype(B) == Complex{BigFloat}
 end
